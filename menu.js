e18b5b25e6bb45d3ffa985fb70470e0905c1ecf3
Kirin Woo <q571@qq.com>
2014-08-20 20:22:49 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,12 +1,13 @@
+document.write('<img id="MenuShow" src="http://www.iconpng.com/png/windows8_icons2/bill.png" style="position: fixed; right: 7%; bottom: 100px; z-index: 998;width: 7%;">');
+document.write('<div id="MenuList" style="display: none;"><ul><li><a href="/">Home</a></li></ul></div>');
+
+
 //LOAD MENU
 //var z = document.createElement('img');
-//z.setAttribute("src", "http://img.taopic.com/uploads/allimg/130515/240469-13051523520152.jpg");
+//z.setAttribute("src", "http://www.iconpng.com/png/windows8_icons2/bill.png");
 //z.setAttribute("style", "position: fixed; right: 7%; bottom: 100px; z-index: 998;width: 7%;");
 //z.setAttribute("id", "MenuShow");
 //document.getElementsByTagName("body")[0].appendChild(z);
-
-document.write('<img id="MenuShow" src="http://img.taopic.com/uploads/allimg/130515/240469-13051523520152.jpg" style="position: fixed; right: 7%; bottom: 100px; z-index: 998;width: 7%;">');
-document.write('<div id="MenuList" style="display: none;"><ul><li><a href="/">Home</a></li></ul></div>');
 
 
 
@@ -24,13 +25,13 @@ document.write('<div id="MenuList" style="display: none;"><ul><li><a href="/">Ho
 var z = document.createElement('link');
 z.setAttribute("rel", "stylesheet");
 z.setAttribute("type", "text/css");
-z.setAttribute("href", "https://raw.githubusercontent.com/artberri/sidr/master/dist/stylesheets/jquery.sidr.dark.css");
+z.setAttribute("href", "http://raw.githubusercontent.com/artberri/sidr/master/dist/stylesheets/jquery.sidr.dark.css");
 document.getElementsByTagName("head")[0].appendChild(z);
 
 var z = document.createElement('script');
 z.setAttribute("type", "text/javascript");
 z.setAttribute("charset", "utf-8");
-z.setAttribute("src", "https://rawgithub.com/artberri/sidr/master/src/jquery.sidr.js");
+z.setAttribute("src", "http://rawgithub.com/artberri/sidr/master/src/jquery.sidr.js");
 document.getElementsByTagName("body")[0].appendChild(z);
 
 
