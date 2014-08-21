9f0a416141fd0f0ca0ac7893145144061104b1b9
Kirin Woo <q571@qq.com>
2014-08-21 19:36:22 +0800

    Update menu.js

--- jc/menu.js
+++ jc/menu.js
@@ -1,24 +1,11 @@
-document.write('<img id="MenuShow" src="http://www.iconpng.com/png/windows8_icons2/bill.png" style="position: fixed; right: 7%; bottom: 100px; z-index: 998;width: 5%;">');
-document.write('<div id="MenuList" style="display: none;"><ul><li><a href="/">Home</a></li><li><a href="/#windows">windows</a></li></ul></div>');
+document.write('/<img id="MenuShow" src="http://www.iconpng.com/png/windows8_icons2/bill.png" style="position: fixed; right: 7%; bottom: 100px; z-index: 998;width: 5%;">');
 
-
-//LOAD MENU
-//var z = document.createElement('img');
-//z.setAttribute("src", "http://www.iconpng.com/png/windows8_icons2/bill.png");
-//z.setAttribute("style", "position: fixed; right: 7%; bottom: 100px; z-index: 998;width: 7%;");
-//z.setAttribute("id", "MenuShow");
-//document.getElementsByTagName("body")[0].appendChild(z);
-
-
-
-//LOAD JQUERY
-//var z = document.createElement('script');
-//z.setAttribute("type", "text/javascript");
-//z.setAttribute("charset", "utf-8");
-//z.setAttribute("src", "http://code.jquery.com/jquery-2.1.1.min.js");
-//document.getElementsByTagName("head")[0].appendChild(z);
-
-
+document.write('
+  <div id="MenuList" style="display: none;"><ul>
+  <li><a href="/">Home</a></li>
+  <li><a href="/#windows">windows</a></li>
+  </ul></div>'
+);
 
 
 //LOAD SIDR
@@ -63,10 +50,8 @@ window.onload = function(){
     
     for (var i=0; i<$('a').length; i++) {
       var _a = $('a')[i];
-      if (_a.href.split("#")[1]) {
-        $(_a).unbind('click').bind('click', function(){
-          ArticleCategory(_a.href.split("#")[1]);
-        });
+      if (_a.href.split("#")[1]) { $(_a).attr("onclick", "ArticleCategory('"+_a.href.split("#")[1]+"')");
+         console.log(_a)
       }
     }
     
