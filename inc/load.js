c69304ad074541f6c785b2ff5930e8ebe3db13b6
qbless <q571@qq.com>
2014-09-03 11:24:22 +0800

    blog

--- inc/load.js
+++ inc/load.js
@@ -85,5 +85,5 @@ if (window.location.href.split("/")[3]=='' || window.location.href.indexOf('/#')
 }
 
 //LOAD CNZZ
-var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253192811'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s95.cnzz.com/stat.php%3Fid%3D1253192811%26online%3D1%26show%3Dline' type='text/javascript'%3E%3C/script%3E"));
+$.getScript("http://s95.cnzz.com/stat.php?id=1253192811&online=1&show=line");
 }
