8018bf63ef0335c60261dcd55731717f12a9da3b
Kirin Woo <q571@qq.com>
2015-04-01 17:53:05 +0800

    Update load.js

--- asset/load.js
+++ asset/load.js
@@ -2,7 +2,7 @@ define(function (require, exports, module) {
   $ = require('jquery');
 
   //根据URL判断加载页面
-  var page = window.location.href.split(window.location.host)[1].substr(1, 6) || 'index';
+  var page = window.location.href.split(window.location.host)[1].substr(1) || 'index';
   
   //先加载marked类
   require.async('marked', function() {
