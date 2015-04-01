cde6f008333a5ee8c156ae9966d0119965d120c5
Kirin Woo <q571@qq.com>
2015-04-01 17:32:55 +0800

    Create init.js

---
+++ asset/init.js
@@ -0,0 +1,15 @@
+//获取根路径放到全局对象G里面
+var tmp = {}, G = {};
+tmp.scripts = document.scripts;
+tmp.current = tmp.scripts[tmp.scripts.length - 1];
+G.baseUrl = tmp.current.src.substring(0, tmp.current.src.lastIndexOf('init.js'));
+tmp = {}; //释放
+
+//seajs 配置
+seajs.config({
+  base  : G.baseUrl,
+  alias : {
+    'jquery' : 'lib/jquery.min',
+    'marked' : 'lib/marked.min'
+  }
+});
