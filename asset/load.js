ad3e7b01c8c8374187838408e7212ca930371308
Kirin Woo <q571@qq.com>
2015-04-01 17:49:16 +0800

    Update load.js

--- asset/load.js
+++ asset/load.js
@@ -2,11 +2,15 @@ define(function (require, exports, module) {
   $ = require('jquery');
 
   //根据URL判断加载页面
-  var page = window.location.href.split(window.location.host)[1].substr(1) || 'index';
+  var page = window.location.href.split(window.location.host)[1].substr(1, 6) || 'index';
   
+  //先加载marked类
   require.async('marked', function() {
-    require.async('md/'+page+'.md', function(content) {
-      $('body').html(marked(content));
+    //再加载样式
+    require.async('theme/base.css', function(content) {
+      require.async('md/'+page+'.md', function(content) {
+        $('body').html(marked(content));
+      });
     });
   });
 });
