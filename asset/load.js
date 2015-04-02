96bda78d0f62670389f7eed1fa72a63bf6200684
Kirin Woo <q571@qq.com>
2015-04-02 09:16:00 +0800

    Update load.js

--- asset/load.js
+++ asset/load.js
@@ -8,17 +8,24 @@ define(function (require, exports, module) {
   page = page.indexOf('.html') < 0 ? page : page.substr(0, page.indexOf('.html'));
   page = page.indexOf('#') < 0 ? page : page.substr(0, page.indexOf('#'));
   page = page || 'index';
-  
+
+  //LOAD ARTICLE
   require.async('md/'+page+'.md', function(content) {
     $('body').html(marked(content));
+  });
 
+  //LOAD CSS
+  require.async('theme/base.min.css');
+  
+  //DELAYED LOAD
+  setTimeout(function() {
     //LOAD CNZZ
     $.getScript("http://s95.cnzz.com/stat.php?id=1253192811");
-    
+
     //Load DUOSUO
     window.duoshuoQuery = {short_name:"qbless"};
-    setTimeout(function(){$('body').append('<div class="ds-thread" data-thread-key="'+page+'" data-title="'+window.document.title+'" data-url="'+window.location.href+'"></div>');$.getScript("http://static.duoshuo.com/embed.js");}, 3*1000);
-  });
-  //加载样式
-  require.async('theme/base.min.css');
+    $('body').append('<div class="ds-thread" data-thread-key="'+page+'" data-title="'+window.document.title+'" data-url="'+window.location.href+'"></div>');
+    $.getScript("http://static.duoshuo.com/embed.js");
+
+  }, 3*1000);
 });
