eda2297c3c10db17097cbc9a24bf26d5f98fb499
Kirin Woo <q571@qq.com>
2015-04-02 08:58:58 +0800

    Update load.js

--- asset/load.js
+++ asset/load.js
@@ -1,5 +1,6 @@
 define(function (require, exports, module) {
   $ = require('jquery');
+  marked = require('marked');
 
   //根据URL判断加载页面
   var page;
@@ -8,20 +9,16 @@ define(function (require, exports, module) {
   page = page.indexOf('#') < 0 ? page : page.substr(0, page.indexOf('#'));
   page = page || 'index';
   
-  //先加载marked类
-  require.async('marked', function() {
-    //再加载样式
-    require.async('theme/base.min.css', function(content) {
-      require.async('md/'+page+'.md', function(content) {
-        $('body').html(marked(content));
-  
-        //LOAD CNZZ
-        $.getScript("http://s95.cnzz.com/stat.php?id=1253192811");
-        
-        //Load DUOSUO
-        window.duoshuoQuery = {short_name:"qbless"};
-        setTimeout(function(){$('body').append('<div class="ds-thread" data-thread-key="'+page+'" data-title="'+window.document.title+'" data-url="'+window.location.href+'"></div>');$.getScript("http://static.duoshuo.com/embed.js");}, 3*1000);
-      });
-    });
+  require.async('md/'+page+'.md', function(content) {
+    $('body').html(marked(content));
+
+    //LOAD CNZZ
+    $.getScript("http://s95.cnzz.com/stat.php?id=1253192811");
+    
+    //Load DUOSUO
+    window.duoshuoQuery = {short_name:"qbless"};
+    setTimeout(function(){$('body').append('<div class="ds-thread" data-thread-key="'+page+'" data-title="'+window.document.title+'" data-url="'+window.location.href+'"></div>');$.getScript("http://static.duoshuo.com/embed.js");}, 3*1000);
   });
+  //加载样式
+  require.async('theme/base.min.css');
 });
