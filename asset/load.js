4567cbec54bd1b84ba0efa4af54939d9fb8e7f4a
Kirin Woo <q571@qq.com>
2015-04-02 14:23:54 +0800

    Update load.js

--- asset/load.js
+++ asset/load.js
@@ -38,6 +38,8 @@ define(function (require, exports, module) {
         category(window.location.hash.substr(1));
       }
       $('a[href^="/#"]').click(function(){category(this.href.split('#')[1])});
+    } else {
+      $('title').html($('h1').html() + ' - ' + $('title').html());
     }
     
     //NON-SITE ADDRESS, OPEN A NEW PAGE
