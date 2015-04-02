1896d8bd39f6a9caa119c08d5450a2342b4fcabb
Kirin Woo <q571@qq.com>
2015-04-02 09:53:05 +0800

    Update load.js

--- asset/load.js
+++ asset/load.js
@@ -2,7 +2,7 @@ define(function (require, exports, module) {
   $ = require('jquery');
   marked = require('marked');
 
-  //根据URL判断加载页面
+  //ACCORDING TO IDENTIFY THE PAGE URL
   var page;
   page = window.location.href.split(window.location.host)[1].substr(1);
   page = page.indexOf('.html') < 0 ? page : page.substr(0, page.indexOf('.html'));
@@ -17,8 +17,29 @@ define(function (require, exports, module) {
   //LOAD CSS
   require.async('theme/base.min.css');
   
+  var category = function(name) {
+    if (!name) {
+      $('a[href^="/#"]').parents('li').show();
+    } else {
+      $('a[href^="/#"]').parents('li').hide();
+      $('a[href^="/#"]').each(function() {
+        if (this.href.indexOf(name) > 0) {
+          $(this).parents('li').show();
+        }
+      })
+    }
+  }
+  
   //DELAYED LOAD
   setTimeout(function() {
+
+    if ('index' == page) {
+      if (window.location.hash.substr(1)) {
+        category(window.location.hash.substr(1));
+      }
+      $('a[href^="/#"]').click(function(){category(this.href.split('#')[1])});
+    }
+
     //LOAD CNZZ
     $.getScript("http://s95.cnzz.com/stat.php?id=1253192811");
 
